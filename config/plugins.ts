fab450e4b06b50e529d9a1d96ea73aed65420fc4
Future <amna@futuredata.me>
2026-02-14 17:57:08 +0400

    Fix Strapi plugins config type

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,6 +1,6 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugins => ({
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
   'users-permissions': {
     config: {
       jwtSecret: env('JWT_SECRET'),
