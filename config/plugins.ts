537e1418a621744dd49c9353d1ead7417c908bcc
Future <amna@futuredata.me>
2026-02-14 17:53:06 +0400

    Configure users-permissions JWT secret via env

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,11 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugins => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET'),
+    },
+  },
+});
 
 export default config;
